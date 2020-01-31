e0dd30518b7656be80941e1fdbb5803ebb0566b5
Flynsarmy <flynsarmy@gmail.com>
2020-01-31 06:10:57 +0000

    Apply fixes from StyleCI

--- Plugin.php
+++ Plugin.php
@@ -1,43 +1,44 @@
-<?php namespace Flynsarmy\ThemeSelecter;
+<?php
 
-use System\Classes\PluginBase;
+namespace Flynsarmy\ThemeSelecter;
+
+use Config;
 use Cookie;
 use Event;
-use Config;
+use System\Classes\PluginBase;
 
 /**
- * ThemeSelector Plugin Information File
+ * ThemeSelector Plugin Information File.
  */
 class Plugin extends PluginBase
 {
+    /**
+     * Returns information about this plugin.
+     *
+     * @return array
+     */
+    public function pluginDetails()
+    {
+        return [
+            'name'        => 'Theme Selecter',
+            'description' => 'Adds a component which provides a URL to set the active theme',
+            'author'      => 'Flynsarmy',
+            'icon'        => 'icon-leaf',
+        ];
+    }
 
-	/**
-	 * Returns information about this plugin.
-	 *
-	 * @return array
-	 */
-	public function pluginDetails()
-	{
-		return [
-			'name'        => 'Theme Selecter',
-			'description' => 'Adds a component which provides a URL to set the active theme',
-			'author'      => 'Flynsarmy',
-			'icon'        => 'icon-leaf'
-		];
-	}
+    public function registerComponents()
+    {
+        return [
+            '\Flynsarmy\ThemeSelecter\Components\Selecter' => 'themeselecter',
+            '\Flynsarmy\ThemeSelecter\Components\Restorer' => 'themerestorer',
+        ];
+    }
 
-	public function registerComponents()
-	{
-		return [
-			'\Flynsarmy\ThemeSelecter\Components\Selecter' => 'themeselecter',
-			'\Flynsarmy\ThemeSelecter\Components\Restorer' => 'themerestorer',
-		];
-	}
-
-	public function boot()
-	{
-		Event::listen('cms.activeTheme', function() {
-			return Cookie::get('cmsActiveTheme', Config::get('cms.activeTheme'));
-		});
-	}
+    public function boot()
+    {
+        Event::listen('cms.activeTheme', function () {
+            return Cookie::get('cmsActiveTheme', Config::get('cms.activeTheme'));
+        });
+    }
 }
